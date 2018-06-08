f6864cd2630420ff6a424846408a192ae2973683
Pablo <pablo1@mailbox.org>
2018-06-08 18:20:39 +0200

    added test file for iptables

---
+++ iptables_test.go
@@ -0,0 +1,76 @@
+package main
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+func TestListRules(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ListRules()
+		})
+	}
+}
+
+func TestBlockServer(t *testing.T) {
+	type args struct {
+		ip string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			BlockServer(tt.args.ip)
+		})
+	}
+}
+
+func TestUnblockServer(t *testing.T) {
+	type args struct {
+		ip string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			UnblockServer(tt.args.ip)
+		})
+	}
+}
+
+func TestCleanup(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Cleanup()
+		})
+	}
+}
